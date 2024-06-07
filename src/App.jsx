4c154ead6b22761c0d4c8e00cc611967c0dd15b0
claudia <claudianataliurena@gmail.com>
2024-06-07 11:34:00 -0300

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="top-center" />
+        <h2>App</h2>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
