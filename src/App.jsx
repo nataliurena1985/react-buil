d8d7e61c4c3274d45fbd817a57676e4a5dabecfd
claudia <claudianataliurena@gmail.com>
2024-06-10 11:14:04 -0300

    estilos de la tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <ToastContainer position="top-center" />
-        <h2>App</h2>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+import "./home.css";
+
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  const loadData = async () => {
+    const response = await axios.get("http://localhost:4000/api/get");
+
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div style={{ marginTop: "150px" }}>
+      <table className="table table-striped">
+        <thead >
+          <tr>
+            <th style={{ textAlign: "center" }}>No.</th>
+            <th style={{ textAlign: "center" }}>Name</th>
+            <th style={{ textAlign: "center" }}>Email</th>
+            <th style={{ textAlign: "center" }}>Contact</th>
+            <th style={{ textAlign: "center" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={item.id}>
+                <th scope="row">{index + 1}</th>
+                <th>{item.name}</th>
+                <th>{item.email}</th>
+                <th>{item.contact}</th>
+                <th>
+                  <Link to={`/update/${item.id}`}>
+                    <button className="btn btn-info ">Edit</button>
+                  </Link>
+                  <button className="btn btn-danger ">Delete</button>
+
+                  <Link to={`/view/${item.id}`}>
+                    <button className="btn btn-warning ">View</button>
+                  </Link>
+                </th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <h2>Home7</h2>
+    </div>
+  );
+};
+
+export default Home;
